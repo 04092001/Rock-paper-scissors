bf63460521a5126760cb1e19d7d4199d0886309a
Freddie Heath <fheath090401@gmail.com>
2023-12-28 16:21:04 +0000

    Creating foundational Javascript for the game

---
+++ script.js
@@ -0,0 +1,20 @@
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
+  ) {
+    console.log("Player Wins!");
+  }
+  if (
+    (computerSelection === "Rock" && playerSelection === "Scissors") ||
+    (computerSelection === "Paper" && playerSelection === "Rock") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper")
+  ) {
+    console.log("Computer Wins!");
+  }
+
+  if (playerSelection === computerSelection) {
+    console.log("Draw");
+  }
+}
