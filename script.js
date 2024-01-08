ea73c9d0452ec32fbb26235b7a51fc60f5bd8b16
Freddie Heath <fheath090401@gmail.com>
2024-01-08 16:59:39 +0000

    Added a reset & winner when the score reaches five

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // GAME
 let playerScore = 0;
 let computerScore = 0;
+let roundCount = 0;
 let playerSelection;
 
 function getPlayerChoice(choice) {
-  if ((choice = "Rock")) {
+  if (choice === "Rock") {
     playerSelection = "Rock";
   }
-  if ((choice = "Paper")) {
+  if (choice === "Paper") {
     playerSelection = "Paper";
   }
-  if ((choice = "Scissors")) {
+  if (choice === "Scissors") {
     playerSelection = "Scissors";
   }
 }
@@ -29,33 +30,56 @@ function getRandomChoice() {
 
 function playRound(playerSelection, computerSelection) {
   computerSelection = getRandomChoice();
-  UIcomputerChoice.textContent = "Computer Chose:" + computerSelection;
+  UIplayerChoice.textContent = "Player Chose: " + playerSelection;
+  UIcomputerChoice.textContent = "Computer Chose: " + computerSelection;
 
   if (
     (playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Scissors" && computerSelection === "Paper") ||
     (playerSelection === "Paper" && computerSelection === "Rock")
   ) {
-    console.log("Player Wins!");
     playerScore++;
-    gameOutcome.textContent = "Game Outcome: Player Wins!";
-    UIplayerScore.textContent = "Player score is: " + playerScore;
+    roundOutcome.textContent = "Round Outcome: Player Wins!";
   }
   if (
     (computerSelection === "Rock" && playerSelection === "Scissors") ||
     (computerSelection === "Paper" && playerSelection === "Rock") ||
     (computerSelection === "Scissors" && playerSelection === "Paper")
   ) {
-    console.log("Computer Wins!");
     computerScore++;
-    gameOutcome.textContent = "Game Outcome: Computer Wins!";
-    UIcomputerScore.textContent = "Computer score is: " + computerScore;
+    roundOutcome.textContent = "Round Outcome: Computer Wins!";
   }
 
   if (playerSelection === computerSelection) {
-    console.log("Draw");
-    gameOutcome.textContent = "Game Outcome: Draw";
+    roundOutcome.textContent = "Round Outcome: Draw";
   }
+
+  if (playerScore === 5 || computerScore === 5) {
+    displayWinner();
+    resetGame();
+  } else {
+    updateScore();
+  }
+}
+
+function displayWinner() {
+  if (playerScore === 5) {
+    roundOutcome.textContent = "Player Wins the Game!";
+  }
+  if (computerScore === 5) {
+    roundOutcome.textContent = "Computer Wins the Game!";
+  }
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  updateScore();
+}
+
+function updateScore() {
+  UIplayerScore.textContent = "Player Score " + playerScore;
+  UIcomputerScore.textContent = "Computer Score: " + computerScore;
 }
 
 // UI
@@ -63,26 +87,22 @@ const UIplayerScore = document.getElementById("playerScore");
 const UIcomputerScore = document.getElementById("computerScore");
 const UIplayerChoice = document.getElementById("playerChoice");
 const UIcomputerChoice = document.getElementById("computerChoice");
-const gameOutcome = document.getElementById("gameOutcome");
+const roundOutcome = document.getElementById("roundOutcome");
 const rockBtn = document.getElementById("rockBtn");
 const paperBtn = document.getElementById("paperBtn");
 const scissorsBtn = document.getElementById("scissorsBtn");
 
 rockBtn.addEventListener("click", () => {
   playRound("Rock");
-  UIplayerChoice.textContent = "Player Choice is: Rock";
 });
 
 paperBtn.addEventListener("click", () => {
   playRound("Paper");
-  UIplayerChoice.textContent = "Player Choice is: Paper";
 });
 
 scissorsBtn.addEventListener("click", () => {
-  playRound("Scissors`");
-  UIplayerChoice.textContent = "Player Choice is: Scissors";
+  playRound("Scissors");
 });
 
-//gameOver()
-
-// Turn console.log()'s into textContent's
+// Make it clearer when the round has changed
+// Track previous rounds below the game with W / L / D etc.
